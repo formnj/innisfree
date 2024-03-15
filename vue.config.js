932ce82cfb09f3d59c770c9bb523f8484d6e8ba8
jhlee <jhlee@rianag.com>
2024-03-15 11:23:59 +0900

    build

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
-
 module.exports = defineConfig({
     transpileDependencies: true,
     lintOnSave : false,
-    publicPath: "/innisfree",
-    outputDir: "/docs"
+    outputDir: './docs',
+    publicPath: '/innisfree/'
 })
