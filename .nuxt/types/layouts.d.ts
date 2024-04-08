cb0fdead36450bf8df66442a53135847cc997f40
jhlee <subdiary@daum.net>
2024-04-08 18:00:40 +0900

    reset

--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, MaybeRef } from 'vue'
-export type LayoutKey = "aa-default" | "aa-guide" | "category" | "default" | "guide" | "mo-category" | "mo-default" | "pc-category" | "pc-default" | "publish" | "publishlnb"
-declare module "../../node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: MaybeRef<LayoutKey | false> | ComputedRef<LayoutKey | false>
-  }
-}
