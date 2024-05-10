5624b3fd09731ecb840bfb5eb21915b6a0cb468d
Hyung-98 <hminwo98@naver.com>
2024-05-10 13:48:14 +0900

    PC 마이페이지 개인정보 수정 작업

--- components/Header/mo.vue
+++ components/Header/mo.vue
@@ -106,7 +106,6 @@
 <script setup>
 import {
     global_menu,
-    sample_goods,
     gnb_list
 } from '~/test/data/publish/dummyData'
 
@@ -198,23 +197,7 @@ onMounted(() => {
             }
         });
     });
-
-    document.querySelector('.search_layer .input input').addEventListener('input', auto_complete);
 });
-
-const isValid = ref('');
-const isBool = ref(false);
-const auto_list = ref([]);
-const prd_list = ref([]);
-
-const auto_complete = (e) => {
-    isValid.value = e.target.value;
-
-    auto_list.value = sample_auto.filter(e => e.text.indexOf(isValid.value) >= 0);
-    prd_list.value = sample_goods.filter(e => e.name !== undefined && e.name.indexOf(isValid.value) >= 0);
-
-    auto_list.value.length <= 0 ? isBool.value = true : isBool.value = false;
-};
 
 const cate_tab = (e) => {
     const lis = [...e.target.parentElement.parentElement.children];
