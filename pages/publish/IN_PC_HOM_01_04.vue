0b9f751744a6241fe1b0d1ae36ab03cb01e09198
hgkim <hgkim@rianag.com>
2024-05-31 11:00:24 +0900

    스크롤 관련 css 수정

--- pages/publish/IN_PC_HOM_01_04.vue
+++ pages/publish/IN_PC_HOM_01_04.vue
@@ -195,9 +195,9 @@ onMounted(() => {
         const target = document.querySelector('.tab_wrap > .type_02');
         const sticky_menu = document.querySelector('.sticky_menu_wrap')
         const y = window.scrollY
-        const offtop = document.querySelector('.title_wrap').offsetTop;
+        const offtop = document.querySelector('.title_wrap').getBoundingClientRect().y;
         console.log('offtop', offtop)
-        if (y >=200) {
+        if (offtop <= 1) {
             target.style.display="none";
             fixed.classList.add('active')
             sticky_menu.style.display="flex";
