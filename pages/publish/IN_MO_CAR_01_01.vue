c9dc78532e7481f196bc551cf755a934d1cea60e
Hyung-98 <hminwo98@naver.com>
2024-06-13 10:28:54 +0900

    common.scss reuplod, mobile cart promotion modal modify

--- pages/publish/IN_MO_CAR_01_01.vue
+++ pages/publish/IN_MO_CAR_01_01.vue
@@ -100,15 +100,150 @@
     </a>
   </div>
   <!-- //custom banner -->
+
+  <!-- 프로모션 모달 -->
+  <div id="modal_promotion" class="modal_wrap active promotion fullMo">
+    <div class="modal_container">
+        <div class="modal_header">
+            <h2>프로모션 제품</h2>
+            <button class="btn_close" @click="modal.close(this);">닫기</button>
+        </div>
+        <div class="modal_content">
+            <section>
+              <button
+                class="promo_tit"
+                type="button"
+                :class="{active: promotion_table}"
+                @click="promotion_table = !promotion_table"
+              >
+                2개 이상 구매 시 최대 [50% 할인]
+              </button>
+              <div v-if="promotion_table" class="table">
+                <table>
+                  <caption>프로모션 제품 설명 및 추가</caption>
+                  <colgroup>
+                    <col width="50%">
+                    <col width="50%">
+                  </colgroup>
+                  <thead>
+                    <tr>
+                      <th>조건</th>
+                      <th>할인액</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1개 구매시</td>
+                      <td>30%</td>
+                    </tr>
+                    <tr>
+                      <td>2개 구매시</td>
+                      <td>50%</td>
+                    </tr>
+                    <tr>
+                      <td>3개 구매시</td>
+                      <td>50%</td>
+                    </tr>
+                    <tr>
+                      <td>4개 구매시</td>
+                      <td>50%</td>
+                    </tr>
+                    <tr>
+                      <td>5개 구매시</td>
+                      <td>50%</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </section>
+
+            <section>
+              <p class="promo_tit">프로모션 제품 더 담기</p>
+              <ul class="cart_list">
+                <li v-for="(item, idx) in sample_goods.slice(0,2)" :key="idx">
+                  <div class="cart_item" :class="{option_type: idx === 0}">
+                    <div class="row">
+                      <div class="prod_img">
+                        <Inputs _type="checkbox" :isDisabled="item.status ? true : false"/>
+                        <a href="#none" class="thumb" :class="item.status">
+                          <img :src="item.img" />
+                        </a>
+                      </div>
+
+                      <div class="prod_info">
+                        <a class="name" href="#none"><strong>{{ item.cate }}</strong> {{ item.name }}</a>
+                        <p v-if="item.hasNoti" class="fc_red">{{ item.hasNoti }}</p>
+                        <button
+                          v-if="item.status && item.status == 'sold_out'"
+                          type="button"
+                          class="btn_text_green"
+                          @click="modal.open('modal_stock_alert','full modal_stock_alert')"
+                        >
+                          입고알림신청
+                        </button>
+                        <ProdSelectbox
+                          v-if="item.hasOption"
+                          _placeholder="옵션을 선택해주세요"
+                          :options="[
+                            { val: 'op1', name: 'op', txt: '베이지' },
+                            { val: 'op2', name: 'op', txt: '베이비핑크' },
+                            { val: 'op3', name: 'op', txt: '상품명 상품명 상품명 상품명 상품명', soldout: true, stockAlert: true },
+                          ]"
+                        />
+                      </div>
+
+                      <div v-if="!item.status || !item.status == 'sold_out'" class="prod_price">
+                        <div class="quantity_area">
+                          <Quantity v-if="!item.hasOption" _id="Quantity" quantity="1" />
+                          <span v-if="item.isLimited" :class="item.isLimited.over ? 'err' : '' ">최대선택{{ item.isLimited.limit }}개까지</span> <!-- 최대선택 갯수 넘을 경우 err 클래스 추가-->
+                        </div>
+                        <div v-if="!item.isSample" class="price_wrap">
+                          <span class="cost">{{ item.cost }}원</span>
+                          <span class="price">{{ item.price }}원</span>
+                        </div>
+                      </div>
+
+                      <span v-if="item.status && item.status == 'sold_out'">일시품절</span> <!-- 상태 : 일시품절, 판매중지, 출시예정 -->
+
+                      <Icons v-if="item.delete || item.delete === undefined" class="btn_del" />
+                    </div>
+
+                    <ul v-if="item.hasOption" class="selected_list">
+                      <li v-for="(list, i) in sample_prod_selected_list.slice(0,3)" :key="i">
+                        <span class="name">{{ list.name}}</span>
+                        <!-- <span v-if="selectedPriceShow" class="price">37,000원</span> -->
+                        <span class="price">{{ list.price }}원 <span class="cost">{{ list.cost }}원</span></span>
+                        <div class="quantity_control">
+                          <div class="count">
+                            <Quantity _id="detail_1" :quantity="1" />
+                          </div>
+                        </div>
+                        <button type="button" class="btn_del">옵션 삭제</button>
+                      </li>
+                    </ul>
+                  </div>
+                </li>
+              </ul>
+            </section>
+        </div>
+        <div class="modal_footer">
+            <Button class="btn_big confirm" txt="장바구니 담기" />
+        </div>
+    </div>
+    <div class="overlay" @click="modal.close(this);"></div>
+  </div>
+  <!-- //프로모션 모달 -->
 </template>
 
 <script setup>
-import { setFilter } from '~/assets/js/common-ui.js'
+import { sample_goods, sample_prod_selected_list } from '~/test/data/publish/dummyData.js'
 import { modal } from '~/assets/js/common-ui.js'
 
 definePageMeta({
 layout:'mo-cart'
 });
+
+const promotion_table = ref(false);
 
 const emit = defineEmits(['title']);
 
