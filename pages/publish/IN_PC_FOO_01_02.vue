125b15d47ed82124b7ffb381696836f4fc10f24c
hgkim <hgkim@rianag.com>
2024-06-14 17:18:49 +0900

    IN_PC_FOO_01_02 작업완료

--- pages/publish/IN_PC_FOO_01_02.vue
+++ pages/publish/IN_PC_FOO_01_02.vue
@@ -293,8 +293,7 @@ section {
         > * + * {
           margin-top:10px;
         }
-        min-width: 500px;
-        height: 320px;
+        min-width:500px;
         padding: 55.5px 20px;
         color: #fff;
         font-size: 14px;
@@ -302,6 +301,7 @@ section {
         line-height: 1.86;
         box-sizing: border-box;
         text-align: center;
+        background-size:cover;
         position: relative;
         display:flex;
         flex-direction:column;
@@ -335,14 +335,14 @@ section {
       }
       > div {
         width:calc(100% - 500px);
-        max-height:320px;;
-        overflow: hidden;
-        .item {
-          overflow:hidden;
-          img {max-height:320px;}
+        .swiper {
+          display: flex;
+          flex-direction: column;
+          gap: 10px;
         }
 
         .swiper-scrollbar-wrap {
+          margin-top:30px;
           position:absolute;
           right:0;
           bottom:0;
